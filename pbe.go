07a6e2847c3255d66fcedf71509a300ea5f7cff9
Coda Hale <coda.hale@gmail.com>
2021-03-16 12:53:32 -0600

    Add marshalling for EncryptedKeyPair.

--- pbe.go
+++ pbe.go
@@ -1,13 +1,19 @@
 package veil
 
 import (
+	"encoding"
+	"encoding/base64"
 	"encoding/binary"
+	"errors"
+	"fmt"
 	"io"
 	"math"
 	"runtime"
 
+	"github.com/bwesterb/go-ristretto"
 	"golang.org/x/crypto/argon2"
 	"golang.org/x/crypto/chacha20poly1305"
+	"golang.org/x/crypto/cryptobyte"
 )
 
 // EncryptedSecretKey is a SecretKey that has been encrypted with a password.
@@ -17,6 +23,76 @@ type EncryptedSecretKey struct {
 	Time, Memory uint32
 	Threads      uint8
 }
+
+func (ekp *EncryptedSecretKey) MarshalBinary() ([]byte, error) {
+	b := cryptobyte.NewBuilder(nil)
+	b.AddUint24LengthPrefixed(func(child *cryptobyte.Builder) {
+		child.AddBytes(ekp.Salt)
+	})
+	b.AddUint24LengthPrefixed(func(child *cryptobyte.Builder) {
+		child.AddBytes(ekp.Ciphertext)
+	})
+	b.AddUint32(ekp.Time)
+	b.AddUint32(ekp.Memory)
+	b.AddUint8(ekp.Threads)
+
+	return b.Bytes()
+}
+
+var ErrInvalidEncryptedKeyPair = errors.New("invalid encrypted key pair")
+
+func (ekp *EncryptedSecretKey) UnmarshalBinary(data []byte) error {
+	var salt, ciphertext cryptobyte.String
+
+	s := cryptobyte.String(data)
+
+	if !(s.ReadUint24LengthPrefixed(&salt) &&
+		s.ReadUint24LengthPrefixed(&ciphertext) &&
+		s.ReadUint32(&ekp.Time) &&
+		s.ReadUint32(&ekp.Memory) &&
+		s.ReadUint8(&ekp.Threads)) {
+		return ErrInvalidEncryptedKeyPair
+	}
+
+	ekp.Salt = salt
+	ekp.Ciphertext = ciphertext
+
+	return nil
+}
+
+func (ekp *EncryptedSecretKey) MarshalText() ([]byte, error) {
+	b, err := ekp.MarshalBinary()
+	if err != nil {
+		return nil, err
+	}
+
+	t := make([]byte, base64.RawURLEncoding.EncodedLen(len(b)))
+	base64.RawURLEncoding.Encode(t, b)
+
+	return t, nil
+}
+
+func (ekp *EncryptedSecretKey) UnmarshalText(text []byte) error {
+	b, err := base64.RawURLEncoding.DecodeString(string(text))
+	if err != nil {
+		return err
+	}
+
+	return ekp.UnmarshalBinary(b)
+}
+
+func (ekp *EncryptedSecretKey) String() string {
+	t, _ := ekp.MarshalText()
+	return string(t)
+}
+
+var (
+	_ encoding.BinaryMarshaler   = &EncryptedSecretKey{}
+	_ encoding.BinaryUnmarshaler = &EncryptedSecretKey{}
+	_ encoding.TextMarshaler     = &EncryptedSecretKey{}
+	_ encoding.TextUnmarshaler   = &EncryptedSecretKey{}
+	_ fmt.Stringer               = &EncryptedSecretKey{}
+)
 
 const (
 	defaultTime   = 1
@@ -77,12 +153,15 @@ func (ekp *EncryptedSecretKey) Decrypt(password []byte) (*SecretKey, error) {
 	}
 
 	// Decode the secret key.
-	var s SecretKey
+	var s ristretto.Scalar
 	if err := s.UnmarshalBinary(b); err != nil {
 		return nil, err
 	}
 
-	return &s, err
+	// Derive the public key.
+	q := sk2pk(&s)
+
+	return &SecretKey{s: s, q: *q}, err
 }
 
 // encodeArgonParams returns the Argon2id params encoded as big-endian integers.
--- pbe_test.go
+++ pbe_test.go
@@ -27,3 +27,105 @@ func TestPBE(t *testing.T) {
 
 	assert.Equal(t, "decrypted keypair", kp.q.Bytes(), dkp.q.Bytes())
 }
+
+func TestEncryptedSecretKey_MarshalBinary(t *testing.T) {
+	t.Parallel()
+
+	ekp := &EncryptedSecretKey{
+		Salt:       []byte("salt"),
+		Ciphertext: []byte("ciphertext"),
+		Time:       10,
+		Memory:     200,
+		Threads:    30,
+	}
+
+	data, err := ekp.MarshalBinary()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, "encoded keypair",
+		[]byte{
+			0x00, 0x00, 0x04, 0x73, 0x61, 0x6c, 0x74, 0x00, 0x00, 0x0a, 0x63, 0x69, 0x70, 0x68,
+			0x65, 0x72, 0x74, 0x65, 0x78, 0x74, 0x00, 0x00, 0x00, 0x0a, 0x00, 0x00, 0x00, 0xc8,
+			0x1e,
+		},
+		data)
+}
+
+func TestEncryptedSecretKey_UnmarshalBinary(t *testing.T) {
+	t.Parallel()
+
+	data := []byte{
+		0x00, 0x00, 0x04, 0x73, 0x61, 0x6c, 0x74, 0x00, 0x00, 0x0a, 0x63, 0x69, 0x70, 0x68,
+		0x65, 0x72, 0x74, 0x65, 0x78, 0x74, 0x00, 0x00, 0x00, 0x0a, 0x00, 0x00, 0x00, 0xc8,
+		0x1e,
+	}
+	want := &EncryptedSecretKey{
+		Salt:       []byte("salt"),
+		Ciphertext: []byte("ciphertext"),
+		Time:       10,
+		Memory:     200,
+		Threads:    30,
+	}
+	got := &EncryptedSecretKey{}
+
+	if err := got.UnmarshalBinary(data); err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, "unmarshaled value", want, got)
+}
+
+func TestEncryptedSecretKey_MarshalText(t *testing.T) {
+	t.Parallel()
+
+	ekp := &EncryptedSecretKey{
+		Salt:       []byte("salt"),
+		Ciphertext: []byte("ciphertext"),
+		Time:       10,
+		Memory:     200,
+		Threads:    30,
+	}
+
+	text, err := ekp.MarshalText()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, "encoded keypair", []byte("AAAEc2FsdAAACmNpcGhlcnRleHQAAAAKAAAAyB4"), text)
+}
+
+func TestEncryptedSecretKey_UnmarshalText(t *testing.T) {
+	t.Parallel()
+
+	text := []byte("AAAEc2FsdAAACmNpcGhlcnRleHQAAAAKAAAAyB4")
+	want := &EncryptedSecretKey{
+		Salt:       []byte("salt"),
+		Ciphertext: []byte("ciphertext"),
+		Time:       10,
+		Memory:     200,
+		Threads:    30,
+	}
+	got := &EncryptedSecretKey{}
+
+	if err := got.UnmarshalText(text); err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, "unmarshaled value", want, got)
+}
+
+func TestEncryptedSecretKey_String(t *testing.T) {
+	t.Parallel()
+
+	ekp := &EncryptedSecretKey{
+		Salt:       []byte("salt"),
+		Ciphertext: []byte("ciphertext"),
+		Time:       10,
+		Memory:     200,
+		Threads:    30,
+	}
+
+	assert.Equal(t, "encoded keypair", "AAAEc2FsdAAACmNpcGhlcnRleHQAAAAKAAAAyB4", ekp.String())
+}
