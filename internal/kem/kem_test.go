03a6ec12afa3e0269395bbf0ec4abe57a44de4bd
Coda Hale <coda.hale@gmail.com>
2021-03-27 19:23:23 -0600

    Add KEM benchmarks.

--- internal/kem/kem_test.go
+++ internal/kem/kem_test.go
@@ -29,3 +29,52 @@ func TestExchange(t *testing.T) {
 
 	assert.Equal(t, "derived secrets", secretA, secretB)
 }
+
+func BenchmarkSend(b *testing.B) {
+	pkA, skA, err := xdh.GenerateKeys()
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	pkB, _, err := xdh.GenerateKeys()
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		_, _, _ = Send(skA, pkA, pkB, []byte("boop"), 20)
+	}
+}
+
+func BenchmarkReceive(b *testing.B) {
+	pkA, skA, err := xdh.GenerateKeys()
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	pkB, skB, err := xdh.GenerateKeys()
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	pkW, _, err := Send(skA, pkA, pkB, []byte("boop"), 20)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		Receive(skB, pkB, pkA, pkW, []byte("boop"), 20)
+	}
+}
+
+func BenchmarkKDF(b *testing.B) {
+	buf := make([]byte, 32)
+
+	for i := 0; i < b.N; i++ {
+		kdf(buf, buf, buf, buf, buf, buf, 64)
+	}
+}
