eae2a2a3339f863d56e77cf4832cbff4ec26da87
Coda Hale <coda.hale@gmail.com>
2021-03-28 13:07:19 -0600

    Using encoding/binary for encrypted secret keys.

--- pbe.go
+++ pbe.go
@@ -5,9 +5,11 @@ import (
 	"crypto/rand"
 	"encoding/binary"
 
+	"github.com/codahale/veil/internal/xdh"
 	"golang.org/x/crypto/argon2"
 	"golang.org/x/crypto/chacha20"
 	"golang.org/x/crypto/chacha20poly1305"
+	"golang.org/x/crypto/poly1305"
 )
 
 // Argon2idParams contains the parameters of the Argon2id passphrase-based KDF algorithm.
@@ -19,24 +21,27 @@ type Argon2idParams struct {
 // EncryptSecretKey encrypts the given secret key with the given passphrase and optional Argon2id
 // parameters. Returns the encrypted key.
 func EncryptSecretKey(sk SecretKey, passphrase []byte, params *Argon2idParams) ([]byte, error) {
+	var esk encryptedSecretKey
+
 	// Use default parameters if none are provided.
 	if params == nil {
 		// As recommended in https://tools.ietf.org/html/draft-irtf-cfrg-argon2-12#section-7.4.
-		params = &Argon2idParams{
+		esk.Params = Argon2idParams{
 			Time:        1,
 			Memory:      1 * 1024 * 1024, // 1GiB
 			Parallelism: 4,
 		}
+	} else {
+		esk.Params = *params
 	}
 
 	// Generate a random salt.
-	salt := make([]byte, saltSize)
-	if _, err := rand.Read(salt); err != nil {
+	if _, err := rand.Read(esk.Salt[:]); err != nil {
 		return nil, err
 	}
 
 	// Use Argon2id to derive a key and nonce from the passphrase and salt.
-	key, nonce := pbeKDF(passphrase, salt, params)
+	key, nonce := pbeKDF(passphrase, esk.Salt[:], &esk.Params)
 
 	// Initialize a ChaCha20Poly1305 AEAD.
 	aead, err := chacha20poly1305.New(key)
@@ -45,34 +50,28 @@ func EncryptSecretKey(sk SecretKey, passphrase []byte, params *Argon2idParams) (
 	}
 
 	// Encrypt the secret key.
-	ciphertext := aead.Seal(nil, nonce, sk, nil)
+	copy(esk.Ciphertext[:], aead.Seal(esk.Ciphertext[:0], nonce, sk, nil))
 
 	// Encode the Argon2id params, the salt, and the ciphertext.
 	buf := bytes.NewBuffer(nil)
-	_ = binary.Write(buf, binary.BigEndian, params.Time)
-	_ = binary.Write(buf, binary.BigEndian, params.Memory)
-	_ = binary.Write(buf, binary.BigEndian, params.Parallelism)
-	_, _ = buf.Write(salt)
-	_, _ = buf.Write(ciphertext)
+	if err := binary.Write(buf, binary.BigEndian, &esk); err != nil {
+		return nil, err
+	}
 
-	return buf.Bytes(), err
+	return buf.Bytes(), nil
 }
 
 // DecryptSecretKey decrypts the given secret key with the given passphrase. Returns the decrypted
 // secret key.
 func DecryptSecretKey(sk, passphrase []byte) (SecretKey, error) {
-	var params Argon2idParams
-
-	// Decode the Argon2id params, the salt, and the ciphertext.
-	buf := bytes.NewReader(sk)
-	_ = binary.Read(buf, binary.BigEndian, &params.Time)
-	_ = binary.Read(buf, binary.BigEndian, &params.Memory)
-	_ = binary.Read(buf, binary.BigEndian, &params.Parallelism)
-	salt := sk[paramPrefixSize : paramPrefixSize+saltSize]
-	ciphertext := sk[paramPrefixSize+saltSize:]
+	// Decode the encrypted secret key.
+	var esk encryptedSecretKey
+	if err := binary.Read(bytes.NewReader(sk), binary.BigEndian, &esk); err != nil {
+		return nil, err
+	}
 
 	// Use Argon2id to re-derive the key and nonce from the passphrase and salt.
-	key, nonce := pbeKDF(passphrase, salt, &params)
+	key, nonce := pbeKDF(passphrase, esk.Salt[:], &esk.Params)
 
 	// Initialize a ChaCha20Poly1305 AEAD.
 	aead, err := chacha20poly1305.New(key)
@@ -81,10 +80,17 @@ func DecryptSecretKey(sk, passphrase []byte) (SecretKey, error) {
 	}
 
 	// Decrypt the secret key.
-	plaintext, err := aead.Open(nil, nonce, ciphertext, nil)
+	plaintext, err := aead.Open(nil, nonce, esk.Ciphertext[:], nil)
 
 	// Return it or any error in decryption.
 	return plaintext, err
+}
+
+// encryptedSecretKey is a fixed-size struct of the encoded values for an encrypted secret key.
+type encryptedSecretKey struct {
+	Params     Argon2idParams
+	Salt       [saltSize]byte
+	Ciphertext [ciphertextSize]byte
 }
 
 // pbeKDF uses Argon2id to derive a ChaCha20Poly1305 key and nonce from the passphrase, salt, and
@@ -97,6 +103,6 @@ func pbeKDF(passphrase, salt []byte, params *Argon2idParams) ([]byte, []byte) {
 }
 
 const (
-	saltSize        = 16
-	paramPrefixSize = 1 + 4 + 4
+	saltSize       = 16
+	ciphertextSize = xdh.SecretKeySize + poly1305.TagSize
 )
