cb3fc5a6b08d75bacc9eb268d0fc72a3d02fc51a
Coda Hale <coda.hale@gmail.com>
2021-04-05 13:29:36 -0600

    Add low-level tests for ristretto255 derivation.

---
+++ pkg/veil/internal/protocols/scaldf/scaldf_test.go
@@ -0,0 +1,38 @@
+package scaldf
+
+import (
+	"testing"
+
+	"github.com/codahale/gubbins/assert"
+	"github.com/codahale/veil/pkg/veil/internal/protocols/rng"
+	"github.com/gtank/ristretto255"
+)
+
+func TestDerivation(t *testing.T) {
+	d0, q0, err := rng.NewEphemeralKeys()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Derive the scalar and element in parallel.
+	d1 := DeriveScalar(d0, "one")
+	q1 := DeriveElement(q0, "one")
+
+	// Calculate what the element should be.
+	q1p := ristretto255.NewElement().ScalarBaseMult(d1)
+
+	assert.Equal(t, "derived elements", q1p.String(), q1.String())
+
+	// Derive an element with a different label.
+	qX := DeriveElement(q0, "two")
+
+	if qX.Equal(q1) == 1 {
+		t.Errorf("%s and %s should not be equal", qX, q1)
+	}
+
+	dX := DeriveScalar(d0, "two")
+
+	if dX.Equal(d1) == 1 {
+		t.Errorf("%s and %s should not be equal", dX, d1)
+	}
+}
